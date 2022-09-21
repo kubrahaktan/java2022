f2d6c9d545abc351ccab3ab8f873a75aed18ba53
kubra <kubra@localhost>
2022-09-21 22:20:04 +0300

    first commit

---
+++ src/intro/main.java
@@ -0,0 +1,37 @@
+package intro;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		//değişken isimlendirmeleri java'da camelCase yazılır
+		String ortaMetin = "İlginizi Çekebilir";
+		String altMetin = "Vade Süresi";
+		
+		System.out.println(ortaMetin);
+		
+		//integer
+		
+		int vade = 12;
+		
+		// ondalıklılar için double
+		
+		double dolarDun = 18.14;
+		double dolarBugun = 18.10;
+		
+		boolean dolarDustuMu = false;
+		
+		String okYonu = "down.svg";
+		
+		if (dolarDun>dolarBugun) { 
+			
+		} else {
+			
+		}
+		
+		
+
+	}
+
+}
